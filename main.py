a6b065c334b4be6b3d636a6ed2aab190012d0306
Aryan <157421289+Aryan-Shekar@users.noreply.github.com>
2025-02-08 20:30:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 import time
 
 def print_intro():
-    print("\nWelcome, brave adventurer! You stand at the entrance of the Dungeon of Rachel.")
-    print("Your quest is to retrieve the Lost Amulet of Eldoria hidden deep within the maze-like depths.")
+    print("\nWelcome, brave adventurer! You stand at the entrance of the Dungeon of Evil Kyle.")
+    print("Your quest is to retrieve the Lost Amulet of Rachel hidden deep within the maze-like depths.")
     print("Choose your actions wisely, for danger lurks around every corner!\n")
 
 def get_choice(options):
